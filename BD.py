884396ac250720f77689aaa8b27d60bbd7ee122e
eileen_balaguera <hobielola05@gmail.com>
2025-05-11 20:11:32 -0500

    Update BD.py

--- BD.py
+++ BD.py
@@ -1,34 +1,33 @@
 class Base_datos:
     def __init__(self):
-        self.lista_datos = [
-            ["P001", "Pequeña", "15 días"],
-            ["P002", "Grande", "5 días"]
-        ]
+        self.lista_datos = []
+        self.produccion_huevos = {}
 
-    def imprimir_info(self):
-        for info_pollo in self.lista_datos:
-            print(info_pollo)
-
-    def agregar_pollo(self, nuevo_pollo):
-        self.lista_datos.append(nuevo_pollo)
-
-    def buscar_pollo(self, codigo):
-        for pollo in self.lista_datos:
-            if pollo[0] == codigo:
-                return pollo
-        return None
+    def guardar_pollo(self, codigo, raza, edad):
+        self.lista_datos.append([codigo, raza, edad])
 
     def actualizar_pollo(self, codigo, nueva_raza, nueva_edad):
         for pollo in self.lista_datos:
             if pollo[0] == codigo:
                 pollo[1] = nueva_raza
                 pollo[2] = nueva_edad
-                return True
-        return False
 
     def eliminar_pollo(self, codigo):
-        for i, pollo in enumerate(self.lista_datos):
-            if pollo[0] == codigo:
-                del self.lista_datos[i]
-                return True
-        return False
+        self.lista_datos = [pollo for pollo in self.lista_datos if pollo[0] != codigo]
+        if codigo in self.produccion_huevos:
+            del self.produccion_huevos[codigo]
+
+    def registrar_huevos(self, codigo, semana, cantidad):
+        if codigo not in self.produccion_huevos:
+            self.produccion_huevos[codigo] = {}
+        self.produccion_huevos[codigo][semana] = cantidad
+
+    def obtener_produccion_total(self, codigo):
+        if codigo in self.produccion_huevos:
+            return sum(self.produccion_huevos[codigo].values())
+        return 0
+
+    def obtener_produccion_por_semana(self, codigo, semana):
+        if codigo in self.produccion_huevos and semana in self.produccion_huevos[codigo]:
+            return self.produccion_huevos[codigo][semana]
+        return 0
