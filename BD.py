43ca08369b244c6b280928761a23afd3650ac6a0
eileen_balaguera <hobielola05@gmail.com>
2025-05-11 19:43:33 -0500

    Update BD.py

--- BD.py
+++ BD.py
@@ -1,8 +1,34 @@
 class Base_datos:
     def __init__(self):
-        self.lista_datos = [["P001" , "Pequeña" , "15dias"], ["P002" , "Grande" , "5dias"]]
-        
+        self.lista_datos = [
+            ["P001", "Pequeña", "15 días"],
+            ["P002", "Grande", "5 días"]
+        ]
+
     def imprimir_info(self):
-        for self.lista_datos in range(self.lista_datos.len):
-            for(info_pollo in self.lista_datos)
+        for info_pollo in self.lista_datos:
             print(info_pollo)
+
+    def agregar_pollo(self, nuevo_pollo):
+        self.lista_datos.append(nuevo_pollo)
+
+    def buscar_pollo(self, codigo):
+        for pollo in self.lista_datos:
+            if pollo[0] == codigo:
+                return pollo
+        return None
+
+    def actualizar_pollo(self, codigo, nueva_raza, nueva_edad):
+        for pollo in self.lista_datos:
+            if pollo[0] == codigo:
+                pollo[1] = nueva_raza
+                pollo[2] = nueva_edad
+                return True
+        return False
+
+    def eliminar_pollo(self, codigo):
+        for i, pollo in enumerate(self.lista_datos):
+            if pollo[0] == codigo:
+                del self.lista_datos[i]
+                return True
+        return False
