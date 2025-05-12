bdd6c82dd67bed97633eb1274e6f178f247670a4
eileen_balaguera <hobielola05@gmail.com>
2025-05-11 19:42:51 -0500

    Update Pollos.py

--- Pollos.py
+++ Pollos.py
@@ -1,29 +1,31 @@
+from bd import Base_datos
+
 class Pollo:
-    
-    #metodos constructores
-    def __init__(self, id_pollo, dato_edad, raza_pollo):
-        #atributos de la clase (get y set)
+    def __init__(self, id_pollo, edad_pollo, raza_pollo):
         self.codigo_pollo = id_pollo
-        self.edad_pollo = dato_edad
+        self.edad_pollo = edad_pollo
         self.raza_pollo = raza_pollo
-        
-        #llamados de otras clases
-        self.objBase_datos = base_datos() # el objeto de la clase
-        
-        
-    #metodos públicos de modificar atributos
+        self.objBase_datos = Base_datos()
+
+    # Métodos Get y Set
     def getCodigo_pollo(self):
         return self.codigo_pollo
-    
+
     def setCodigo_pollo(self, codigo_pollo):
         self.codigo_pollo = codigo_pollo
-        
+
     def getEdad_pollo(self):
         return self.edad_pollo
-    
+
     def setEdad_pollo(self, edad_pollo):
         self.edad_pollo = edad_pollo
-        
-    #Metodos para conectar base datos
+
+    def getRaza_pollo(self):
+        return self.raza_pollo
+
+    def setRaza_pollo(self, raza_pollo):
+        self.raza_pollo = raza_pollo
+
+    # Método para guardar el pollo en base de datos
     def guardar_pollo(self):
-        
+        self.objBase_datos.agregar_pollo([self.codigo_pollo, self.raza_pollo, self.edad_pollo])
